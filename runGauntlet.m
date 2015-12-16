fbb484ae96219d0aebfc7049ddf19941bc2c86d2
Kaliel Williamson <kalielwilliamson@gmail.com>
2015-12-16 10:08:49 -0500

    Create runGauntlet.m

---
+++ runGauntlet.m
@@ -0,0 +1,7 @@
+close all;
+clear all;
+%images without consistently shaped image should not use space hist.
+%ImageNest('images\blackcircle.jpg',3)
+ImageNest('images\green-triangle.jpg',3)
+%ImageNest('images\television.jpg',2)
+beep
