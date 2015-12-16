e93d4206ab0a18cb2446cd7dde2d7fda6b47ea53
Kaliel Williamson <kalielwilliamson@gmail.com>
2015-12-16 10:06:44 -0500

    Create ImageNest.m

---
+++ ImageNest.m
@@ -0,0 +1,44 @@
+function ImageNest(imgg,setting)
+    tic;
+    temp = strsplit(imgg,'\');
+    temp2 = temp(2);
+    temp3 = rptgen.toString(temp2);
+    temp3 = strsplit(temp3,'.');
+    temp4 = temp3(1);
+    name = rptgen.toString(temp4);
+    if setting == 3
+        %shape histogram, horizontal
+        str = 'Horizontal Shape';
+        pic = strcat(name,str);
+        H = getHistogram(imgg,2);
+        multinesting(H(1,:),2,50,pic,str);
+
+        %shape histogram, vertical
+        str = 'Vertical Shape';
+        pic = strcat(name,str);
+        H = getHistogram(imgg,2);
+        multinesting(H(2,:),2,50,pic,str);
+    end
+    
+    if setting == 1 || setting == 2 || setting == 3
+        %luminocity histogram
+        str = 'Luminance';
+        pic = strcat(name,str);
+        H = getHistogram(imgg,1);
+        multinesting(H(:,1),1,50,pic,str);
+    end
+    
+    if setting == 2 || setting == 3
+        %color U histogram
+        str = 'Blue-Luminance';
+        pic = 'redapple_blueluminance';
+        H = getHistogram(imgg,1);
+        multinesting(H(:,2),1,50,pic,str);
+        %color V histogram
+        str = 'Red-Luminance';
+        pic = 'redapple_redluminance';
+        multinesting(H(:,3),1,50,pic,str);
+    end
+    time = toc;
+    sprintf('elapsed time (s): %g',time)
+end
